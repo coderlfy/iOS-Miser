0161f842d6a1f072d6e7bad623d12201fca67c97
李峰艳 <a@rihouyandeMacBook-Air.local>
2015-08-08 15:40:46 +0800

    ds

--- IosMiser/EditConsumptionController.h
+++ IosMiser/EditConsumptionController.h
@@ -19,7 +19,7 @@ typedef void(^ButBlock)(ConsumptionModel *consumption, BOOL isAdd);
 @property (strong, readwrite, nonatomic) RETableViewSection *basicControlsSection;
 @property (strong, readwrite, nonatomic) RETableViewSection *buttonSection;
 @property (nonatomic,copy) ButBlock block;
-//测试12356223
+//测试1235622312
 - (instancetype) initByAddUI;
 - (instancetype) initByUpdateUI:(ConsumptionModel *)consumption;
 @end
