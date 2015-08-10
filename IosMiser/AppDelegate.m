6c833d598254ade64955599633cb600d3f1b44b0
李峰艳 <a@rihouyandeMacBook-Air.local>
2015-08-10 10:44:23 +0800

    rt

--- IosMiser/AppDelegate.m
+++ IosMiser/AppDelegate.m
@@ -15,7 +15,7 @@
 
 @implementation AppDelegate
 
-//测试
+//测试123
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
